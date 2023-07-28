dcdfae911451d37ca0f08d2bf8d4cff5bd877c2f
Stephen Allen <sallen@semantic-ai.com>
2023-07-27 20:54:29 -0400

    Parse escape sequences in Cypher names

--- cypher2sql/src/main/java/org/cytosm/cypher2sql/cypher/ast/expression/PropertyKeyName.java
+++ cypher2sql/src/main/java/org/cytosm/cypher2sql/cypher/ast/expression/PropertyKeyName.java
@@ -1,6 +1,7 @@
 package org.cytosm.cypher2sql.cypher.ast.expression;
 
 import org.cytosm.cypher2sql.cypher.ast.ASTNode;
+import org.cytosm.cypher2sql.utils.StringEscapeUtils;
 
 /**
  */
@@ -8,7 +9,8 @@ public class PropertyKeyName extends ASTNode {
 
     public String name;
 
-    public PropertyKeyName(final String name) {
-        this.name = name;
+    public PropertyKeyName(final String value) {
+        final String str = value.startsWith("`") ? value.substring(1, value.length() - 1) : value;
+        this.name = StringEscapeUtils.unescapeCypherName(str);
     }
 }
--- cypher2sql/src/main/java/org/cytosm/cypher2sql/cypher/ast/expression/Variable.java
+++ cypher2sql/src/main/java/org/cytosm/cypher2sql/cypher/ast/expression/Variable.java
@@ -1,12 +1,15 @@
 package org.cytosm.cypher2sql.cypher.ast.expression;
 
+import org.cytosm.cypher2sql.utils.StringEscapeUtils;
+
 /**
  */
 public class Variable extends Expression {
 
     public String name;
 
-    public Variable(final String name) {
-        this.name = name;
+    public Variable(final String value) {
+        final String str = value.startsWith("`") ? value.substring(1, value.length() - 1) : value;
+        this.name = StringEscapeUtils.unescapeCypherName(str);
     }
 }
